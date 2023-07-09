baca5350f4450abc9c390f704bbdca6a5630b12d
Zhang Minghan <zmh13054618081@dingtalk.com>
2023-07-09 09:41:09 +0800

    update auth token

--- backend/auth.go
+++ backend/auth.go
@@ -2,8 +2,41 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
+	"github.com/dgrijalva/jwt-go"
 	"github.com/gin-gonic/gin"
+	"github.com/go-redis/redis/v8"
+	"github.com/spf13/viper"
+	"time"
 )
+
+type User struct {
+	ID       int64  `json:"id"`
+	Username string `json:"username"`
+	BindID   int64  `json:"bind_id"`
+	Password string `json:"password"`
+	Token    string `json:"token"`
+}
+
+func (u *User) Validate(c *gin.Context) bool {
+	if u.Username == "" || u.Password == "" {
+		return false
+	}
+	cache := c.MustGet("cache").(*redis.Client)
+
+	if password, err := cache.Get(c, fmt.Sprintf("note:user:%s", u.Username)).Result(); err == nil && len(password) > 0 {
+		return u.Password == password
+	}
+
+	db := c.MustGet("db").(*sql.DB)
+	var count int
+	if err := db.QueryRow("SELECT COUNT(*) FROM auth WHERE username = ? AND password = ?", u.Username, u.Password).Scan(&count); err != nil || count == 0 {
+		return false
+	}
+
+	cache.Set(c, fmt.Sprintf("note:user:%s", u.Username), u.Password, 30*time.Minute)
+	return true
+}
 
 func IsUserExist(db *sql.DB, username string) bool {
 	var count int
@@ -11,6 +44,42 @@ func IsUserExist(db *sql.DB, username string) bool {
 		return false
 	}
 	return count > 0
+}
+
+func GenerateToken(username, password string) string {
+	instance := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"username": username,
+		"password": password,
+		"exp":      time.Now().Add(time.Hour * 24 * 30).Unix(),
+	})
+	token, err := instance.SignedString([]byte(viper.GetString("secret")))
+	if err != nil {
+		return ""
+	}
+	return token
+}
+
+func ParseToken(c *gin.Context, token string) *User {
+	instance, err := jwt.Parse(token, func(token *jwt.Token) (interface{}, error) {
+		return []byte(viper.GetString("secret")), nil
+	})
+	if err != nil {
+		return nil
+	}
+	if claims, ok := instance.Claims.(jwt.MapClaims); ok && instance.Valid {
+		if claims["exp"].(int64) < time.Now().Unix() {
+			return nil
+		}
+		user := &User{
+			Username: claims["username"].(string),
+			Password: claims["password"].(string),
+		}
+		if !user.Validate(c) {
+			return nil
+		}
+		return user
+	}
+	return nil
 }
 
 func Login(c *gin.Context, token string) bool {
@@ -24,7 +93,7 @@ func Login(c *gin.Context, token string) bool {
 		password := GenerateChar(64)
 		_ = db.QueryRow("INSERT INTO auth (bind_id, username, token, password) VALUES (?, ?, ?, ?)",
 			user.ID, user.Username, token, password)
-		
+
 	}
 	return true
 }
--- backend/go.mod
+++ backend/go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/bytedance/sonic v1.9.2 // indirect
 	github.com/cespare/xxhash/v2 v2.1.2 // indirect
 	github.com/chenzhuoyu/base64x v0.0.0-20221115062448-fe3a3abad311 // indirect
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/fsnotify/fsnotify v1.6.0 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.2 // indirect
