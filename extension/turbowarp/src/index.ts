46b7205366b74250b428ac6fad1ad1c35bddaa6a
Zhang Minghan <zmh13054618081@dingtalk.com>
2023-07-20 22:49:03 +0800

    update i18n

--- extension/turbowarp/src/index.ts
+++ extension/turbowarp/src/index.ts
@@ -8,6 +8,7 @@ new Extension({
     opcode: "send",
     blockType: "reporter",
     text: "发送随记便签 标题 [title:string] 内容 [body:string] (漂流瓶特有的会丢失~)",
+    default: { title: "新便签", body: "写点什么" },
     bind: async function ({ title, body }): Promise<string> {
       if (!title || !body) {
         return "标题或内容不能为空"
@@ -21,8 +22,8 @@ new Extension({
           body: JSON.stringify({ title, body }),
         });
         const data = await res.json();
-        if (!data.data.status) return "发送失败！请稍后重试";
-        return data.data.code;
+        if (!data.status) return "发送失败！请稍后重试";
+        return data.code;
       } catch (e) {
         console.debug(e);
         return "发送失败！请检查您的网络环境";
@@ -39,15 +40,24 @@ new Extension({
       try {
         const res = await fetch(`https://notes.lightxi.com/api/anonymous/get?code=${code}`);
         const data = await res.json();
-        if (!data.data.status) return "接收失败！请检查您的接签码是否正确，匿名便签是否过期";
+        if (!data.status) return "接收失败！请检查您的接签码是否正确，匿名便签是否过期";
         return {
-          title: data.data.title,
-          body: data.data.body,
+          title: data.title,
+          body: data.body,
         };
       } catch (e) {
         console.debug(e);
         return "接收失败！请检查您的网络环境";
       }
     }
-  }]
+  }, {
+    opcode: "website",
+    blockType: "command",
+    text: "寻找河流的源头...",
+    bind: () => window.open("https://notes.lightxi.com"),
+  }],
+  i18n: {
+    source: "zh",
+    accept: ["zh", "en"],
+  }
 }).register()
